73a6bfc87b2ee12fa9ba5925f65d8606fbb191e1
delia paternina <delia.paternina@gmail.com>
2019-04-15 11:49:59 +0100

    Dog details modified

--- fs-furever/src/components/dogs/Dog.js
+++ fs-furever/src/components/dogs/Dog.js
@@ -1,25 +1,11 @@
 import React  from 'react';
 
 const Dog = (props) => {
-  if(!props.dog){
-    return null;
-  }
-
-var owner = [];
-
-  if(props.dog.owner){
-  owner = props.dog.owner.map(element => {
-    return <p>{element.owner}</p>
-  })
-}
 
   return (
     <div className="component">
     <p className="name">
     {props.dog.name}
-    </p>
-    <p className="owner">
-    {props.dog.owner}
     </p>
     </div>
   )
