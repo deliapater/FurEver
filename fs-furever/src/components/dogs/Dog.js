9f806f843854112cc594741cf9ac5fea38e8a864
Kimberley Clark <kimjanehunter@hotmail.com>
2019-04-15 12:24:27 +0100

    Adding picture and location to DogList

--- fs-furever/src/components/dogs/Dog.js
+++ fs-furever/src/components/dogs/Dog.js
@@ -7,6 +7,11 @@ const Dog = (props) => {
     <p className="name">
     {props.dog.name}
     </p>
+    <img src = {props.dog.picture} alt="Image" class="expand"/>
+    <p className="location">{props.dog.owner.location}</p>
+
+
+
     </div>
   )
 }
